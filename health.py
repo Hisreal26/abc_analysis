6d7be04cb38101e9c6a5a95cb2ac3f746c2f2d0c
AKINBAMI OLAOLUWA ISRAEL <oakinbamiisreal@gmail.com>
2025-06-13 16:17:34 +0100

    Update health.py

--- health.py
+++ health.py
@@ -4,6 +4,5 @@ import matplotlib.pyplot as pt
 import numpy as np
 import seaborn as sns
 
-
-pf = pd.read_csv("cancer_probabilities.csv")
+df = pd.read_csv("cancer_probabilities.csv")
 st.write(df.head())
