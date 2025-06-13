34a782d87baef695149f6f3f8854980059928a56
AKINBAMI OLAOLUWA ISRAEL <oakinbamiisreal@gmail.com>
2025-06-13 15:33:22 +0100

    Create health.py

---
+++ health.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as pt
+import numpy as np
+import seaborn as sns
+
+
+pf = pd.read_csv("cancer_probabilities.csv")
+st.write(df.head(10))
